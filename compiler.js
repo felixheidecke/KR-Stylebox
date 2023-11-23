2657aadccd16ba4cad49c472ba0bc83343c5dc1f
Felix Heidecke <felix@heidecke.me>
2023-11-23 11:54:12 +0100

    Initial commit

---
+++ compiler.js
@@ -0,0 +1,23 @@
+const sass = require('sass')
+const fs = require('fs')
+
+compile('src/style.scss', 'dist/style.css')
+compile('src/style.scss', 'dist/style.min.css', 'compressed')
+
+/**
+ * Compile SCSS
+ * @param {string} file input path
+ * @param {string} output output path
+ * @param {'expanded'|'compressed'} outputStyle
+ */
+
+function compile(file, output, outputStyle = 'expanded') {
+	sass.render({ file, outputStyle }, (err, result) => {
+		if (err) {
+			console.error(`Fehler beim Kompilieren von ${file}: ${err}`)
+		} else {
+			fs.writeFileSync(output, result.css)
+			console.log(`Erfolgreich kompiliert: ${file} -> ${output}`)
+		}
+	})
+}
